25226cfb0bf1ab1d91683971da170a6186bf5a1c
luck20yan <Luck7869yan>
2019-11-17 19:57:59 +0200

    some fix

--- client/main.py
+++ client/main.py
@@ -26,41 +26,42 @@ winy = 500
 win = pygame.display.set_mode((winx, winy))
 
 def main():
-    connection.action("active_item_change", data=1)
-
     CameraY = 0
     CameraX = 0
-
+    MixingY = 0
+    MixingX = 0
     run = True
     stopped_v = True
     stopped_h = True
 
     while run:
         c_player = None
+
         while not world.data:
             pass
         # pprint(world.data)
         for i in world.data["players"]:
             if i["name"] == NAME:
                 c_player = i
-        CameraX = c_player['x'] - winx / 2
-        CameraY = c_player['y'] - winy / 2
-        keys = pygame.key.get_pressed()
+        CameraX = c_player['x'] - winx / 2 + MixingX
+        CameraY = c_player['y'] - winy / 2 + MixingY
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if event.button == 4:
-                    if c_player['active_item'] < 8:
+                    if c_player['active_item'] <= 8:
                         connection.action("active_item_change", data=c_player['active_item']+1)
                     else:
                         connection.action("active_item_change", data=0)
                 if event.button == 5:
-                    if c_player['active_item'] > 0:
+                    if c_player['active_item'] >= 0:
                         connection.action("active_item_change", data=c_player['active_item']-1)
                     else:
                         connection.action("active_item_change", data=8)
-        print(c_player['active_item'])
+
+        keys = pygame.key.get_pressed()
+
         if pygame.mouse.get_pressed()[0]:
             connection.action("action", data={"x": pygame.mouse.get_pos()[0], "y": pygame.mouse.get_pos()[1]})
         if keys[pygame.K_F10]:
@@ -76,6 +77,7 @@ def main():
             stopped_v = False
         if keys[pygame.K_s] and c_player['y'] < winy - 40 and stopped_v:
             connection.action("down")
+
             stopped_v = False
         if not (keys[pygame.K_w] or keys[pygame.K_s]) and not stopped_v:
             connection.action("stop", "vertical")
@@ -83,7 +85,10 @@ def main():
         if not (keys[pygame.K_a] or keys[pygame.K_d]) and not stopped_h:
             connection.action("stop", "horizontal")
             stopped_h = True
-
+        i = 0
+        while i <= 500:
+            pygame.draw.rect(win, (100, 100, 100), (winx / 2 - 225 + i-CameraX, 100-CameraY, 50, 50), 2)
+            i += 50
         for player in world.data["players"]:
             x = player['x'] - CameraX
             y = player['y'] - CameraY
@@ -93,7 +98,7 @@ def main():
             image = pygame.transform.scale(pygame.image.load("assets/players/eyeL.png").convert_alpha(), (40, 40))
             win.blit(image, (x, y))
             win.blit(font.render(player['name'], False, (255, 255, 255)), (x, y - 30))
-
+        win.blit(font.render(f"invent: "+str(c_player['active_item']), False, (255, 255, 255)), (winx - 200, 40))
         interface(c_player)
         pygame.display.update()
         pygame.time.delay(1)
@@ -103,7 +108,15 @@ def main():
 def interface(c_player):
     if DEBUG:
         win.blit(font.render(f"x: {c_player['x']}; y: {c_player['y']}", False, (255, 255, 255)), (winx - 200, 20))
+
     pygame.draw.rect(win, (232, 81, 81), (0, int(winy - 20), c_player["hp"] / 100 * winx, 20))
+    i = 0
+
+    while i <= 8:
+           pygame.draw.rect(win, (100, 100, 100), (winx/2-225+i*50, 0, 50, 50), 2)
+           if i ==  c_player['active_item']:
+               pygame.draw.rect(win, (227, 70, 76), (winx / 2 - 225 + i * 50, 0, 50, 50), 2)
+           i+=1
 
 
 main()
